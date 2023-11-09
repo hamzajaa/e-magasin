7a30d9779f38797b3930595b35331d8a48ed9b81
reda souak <souak47@gmail.com>
2023-11-09 15:44:08 +0100

    Etat de paiment Service impl

---
+++ src/main/java/com/jobintech/themain5/emagasin/service/impl/EtatPaiementServiceImpl.java
@@ -0,0 +1,34 @@
+package com.jobintech.themain5.emagasin.service.impl;
+
+import com.jobintech.themain5.emagasin.entity.EtatPaiement;
+import com.jobintech.themain5.emagasin.service.facade.EtatPaiementService;
+
+import java.util.List;
+
+public class EtatPaiementServiceImpl implements EtatPaiementService {
+
+    @Override
+    public List<EtatPaiement> findAll() {
+        return null;
+    }
+
+    @Override
+    public EtatPaiement findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public EtatPaiement save(EtatPaiement etatPaiement) {
+        return null;
+    }
+
+    @Override
+    public EtatPaiement update(EtatPaiement etatPaiement, Long id) {
+        return null;
+    }
+
+    @Override
+    public int delete(Long id) {
+        return 0;
+    }
+}
