300e1de0309e19bd44e98385bde85123d19ba6fe
HAMZA JAA <hamzajaa2017@gmail.com>
2023-11-09 12:15:51 +0100

    Initial commit

---
+++ src/main/java/com/jobintech/themain5/emagasin/EMagasinApplication.java
@@ -0,0 +1,13 @@
+package com.jobintech.themain5.emagasin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EMagasinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EMagasinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jobintech/themain5/emagasin/EMagasinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jobintech.themain5.emagasin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMagasinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
