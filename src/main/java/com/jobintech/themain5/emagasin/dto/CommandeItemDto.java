d8576830640d5e4334586b3da3267198554845c4
ayman guaiche <guaicheayman@gmail.com>
2023-11-10 12:01:47 +0100

    CommandeItemDto 02

--- src/main/java/com/jobintech/themain5/emagasin/dto/CommandeItemDto.java
+++ src/main/java/com/jobintech/themain5/emagasin/dto/CommandeItemDto.java
@@ -2,5 +2,5 @@ package com.jobintech.themain5.emagasin.dto;
 
 import java.math.BigDecimal;
 
-public record CommandeItemDto(Long id, CommandeDto commandeDto, ProductDto productDto, int quantity, BigDecimal prix) {
+public record CommandeItemDto(Long id, CommandeDto commandeDto, ProduitDto productDto, int quantity, BigDecimal prix) {
 }
